55ea351b3bf3fce0a14f4626965fd4a1ee21346e
taisyu shibata <t_shiba117@yahoo.co.jp>
2023-12-29 21:53:45 +0900

    Edit pub node

--- keyboard_teleop/publisher_node.py
+++ keyboard_teleop/publisher_node.py
@@ -1,33 +1,33 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+from std_msgs.msg import Int32
 from pynput import keyboard
 
 class KeyboardPublisher(Node):
     def __init__(self):
         super().__init__('keyboard_publisher')
-        self.publisher_ = self.create_publisher(String, '/motor_control', 10)
+        self.publisher_ = self.create_publisher(Int32, '/motor_control', 10)
         self.listener = keyboard.Listener(on_press=self.on_press)
         self.listener.start()
 
     def on_press(self, key):
         if key == keyboard.Key.up:
-            self.publish_command("forward")
+            self.publish_command(1)  # forward
         elif key == keyboard.Key.down:
-            self.publish_command("backward")
+            self.publish_command(2)  # backward
         elif key == keyboard.Key.left:
-            self.publish_command("left")
+            self.publish_command(3)  # left
         elif key == keyboard.Key.right:
-            self.publish_command("right")
+            self.publish_command(4)  # right
         elif key == keyboard.Key.esc:
-            self.publish_command("stop")
+            self.publish_command(0)  # stop
             return False
 
     def publish_command(self, command):
-        msg = String()
+        msg = Int32()
         msg.data = command
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % command)
+        self.get_logger().info('Publishing: "%d"' % command)
 
 def main(args=None):
     rclpy.init(args=args)
